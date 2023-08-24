34e9abdbf01e474b7fa6736ee9dd1d6c0302204e
vshiva3 <109163327+vshiva3@users.noreply.github.com>
2023-08-24 19:16:03 +0530

    commit

---
+++ Java Projects/Account Management in 3 Layer Service/Service/TradeAccountService.java
@@ -0,0 +1,12 @@
+package Service;
+
+import java.math.BigDecimal;
+
+public interface TradeAccountService {
+    
+    public void deposit(String id, BigDecimal amount);
+    public void withdraw(String id, BigDecimal amount);
+
+    
+    
+}
